05ac24c122b8faa002d00ed8431ca3b7ffcc9eaf
Shereen <shereenfinney@gmail.com>
2017-05-06 19:49:31 +0100

    FIRST RED-GREEN-REFACTOR CYCLE COMPLETE
    Initial feature and unit tests complete for user story 1
    Next to add more detail in unit tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,9 @@
 require "Game"
 
 describe Game do
+  subject(:game) { described_class.new }
   # it { is_expected.to respond_to(:allocateMarkerTo).with(1).argument }
   it "allocates a marker to a player" do
-    expect(subject).to respond_to(:allocateMarkerTo).with(1).argument
+    expect(game).to respond_to(:allocateMarkerTo).with(1).argument
   end
 end
